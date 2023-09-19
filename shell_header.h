032479be476c0a4b3430521ca116808e471dc95b
Allen <tkmzor@gmail.com>
2023-09-19 04:32:18 -0700

    shell_header.h

---
+++ shell_header.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_HEADER_H
+#define SHELL_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* Prototypes */
+int execute_command(char *cmd);
+
+
+#endif
