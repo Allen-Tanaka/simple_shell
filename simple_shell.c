f4477573096da0d8044f531d0c119c9475f282c4
Allen <tkmzor@gmail.com>
2023-09-19 04:14:35 -0700

    simple_shell.c

---
+++ simple_shell.c
@@ -0,0 +1,37 @@
+#include "shell_header.h"
+
+/**
+ * main - Entry point for the shell
+ * Return: 0 on success, 1 on failure
+ */
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read = getline(&line, &len, stdin);
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+
+		if (read == -1)
+		{
+			if (feof(stdin))
+				exit(EXIT_SUCCESS);
+			else
+			{
+				perror("getline");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (read > 1)
+		{
+			line[read - 1] = '\0';
+			execute_command(line);
+		}
+	}
+	free(line);
+	return (0);
+}
