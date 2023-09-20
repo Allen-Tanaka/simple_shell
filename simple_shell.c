4da0487b7ed6e213abf2ac5917d8d7866798cb4c
Allen <tkmzor@gmail.com>
2023-09-20 01:04:37 -0700

    shell fix

--- simple_shell.c
+++ simple_shell.c
@@ -4,25 +4,22 @@
  * main - Entry point for the shell
  * Return: 0 on success, 1 on failure
  */
-
+i
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read = getline(&line, &len, stdin);
-	int interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
-		if (interactive)
-			write(STDOUT_FILENO, "$ ", 2);
+		write(STDOUT_FILENO, "$ ", 2);
 
 		if (read == -1)
 		{
 			if (feof(stdin))
 			{
-				if (interactive)
-					write(STDOUT_FILENO, "\n", 1);
+				write(STDOUT_FILENO, "\n", 1);
 				free(line);
 				exit(EXIT_SUCCESS);
 			}
@@ -39,9 +36,6 @@ int main(void)
 			line[read - 1] = '\0';
 			exec_command(line);
 		}
-
-		if (!interactive)
-			break;
 	}
 	free(line);
 	return (0);
