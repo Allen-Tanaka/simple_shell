81a815c4369ffc4a7e2e33d1ed33345c68ef92c5
Allen <tkmzor@gmail.com>
2023-09-22 02:56:15 -0700

    path_handler.c

---
+++ path_handler.c
@@ -0,0 +1,45 @@
+#include "shell_header.h"
+
+/**
+ * get_command_path - searches the PATH
+ * @cmd: command to search for
+ *
+ * Return: full path of the command or NULL if not found
+ */
+char *get_command_path(char *cmd)
+{
+	char *PATH = getenv("PATH");
+	char *dir, *save_ptr, *full_path;
+	struct stat st;
+	size_t dir_len, cmd_len;
+	char *temp = full_path;
+
+	if (PATH == NULL)
+		return (NULL);
+
+	PATH = strdup(PATH);
+	cmd_len = custom_strlen(cmd);
+
+	for (dir = strtok_r(PATH, ":", &save_ptr); dir; dir = strtok_r
+			(NULL, ":", &save_ptr))
+	{
+		dir_len = custom_strlen(dir);
+		full_path = malloc(dir_len + cmd_len + 2);
+
+		while (*dir)
+			*temp++ = *dir++;
+		*temp++ = '/';
+		while (*cmd)
+			*temp++ = *cmd++;
+		*temp = '\0';
+
+		if (access(full_path, X_OK) == 0)
+		{
+			free(PATH);
+			return (full_path);
+		}
+		free(full_path);
+	}
+	free(PATH);
+	return (NULL);
+}
