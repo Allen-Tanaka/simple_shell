4fa8704bb96276d9e7faac491dac03d964f6c2fb
Allen <tkmzor@gmail.com>
2023-09-22 03:20:47 -0700

    fix declaration in exec.c

--- exec.c
+++ exec.c
@@ -25,7 +25,7 @@ int exec_command(char *cmd)
 {
 	pid_t pid = fork();
 	int status, i = 0;
-	char *argv[100], *token = strtok(cmd, " ");
+	char *argv[100], *token = strtok(cmd, " "), *path_cmd = NULL;
 
 	while (token != NULL)
 	{
